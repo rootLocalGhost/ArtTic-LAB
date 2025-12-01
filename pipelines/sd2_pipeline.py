71dd803dcd2b1bed3cc727c2172cb30a87225e31
Md Siam Mia <mdsiammia.man@gmail.com>
2025-12-01 15:16:26 +0600

    Add img2img support to SD 2 pipeline

--- pipelines/sd2_pipeline.py
+++ pipelines/sd2_pipeline.py
@@ -1,12 +1,16 @@
-# pipelines/sd2_pipeline.py
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
 from .base_pipeline import ArtTicPipeline
 
 
 class SD2Pipeline(ArtTicPipeline):
+    def __init__(self, model_path, dtype=None):
+        super().__init__(model_path, dtype)
+        self.t2i_class = StableDiffusionPipeline
+        self.i2i_class = StableDiffusionImg2ImgPipeline
+
     def load_pipeline(self, progress):
         progress(0.2, "Loading StableDiffusionPipeline (v2)...")
-        self.pipe = StableDiffusionPipeline.from_single_file(
+        self.pipe = self.t2i_class.from_single_file(
             self.model_path,
             torch_dtype=self.dtype,
             use_safetensors=True,
